e176a515303d7a7ea5c78f27e10b3ec9f775f25f
Bazhanov Vasiliy <karas67@yandex.ru>
2018-12-05 13:24:57 +0200

    Добавил изменения

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+window.onload = function(){
+
 $(document).ready(function() {
     $('.slider').lightSlider({
         adaptiveHeight:true,
@@ -8,19 +10,57 @@ $(document).ready(function() {
     });
 });
 
-window.onload = function(){
-    var menuElem = document.getElementById('dropdown-menu'),
-    titleElem = menuElem.querySelector('.title');
-    document.onclick = function(event) {
-        var target = elem = event.target;
-        while (target != this) {
-            if (target == menuElem) {
-                if(elem.tagName == 'A') titleElem.innerHTML = elem.textContent;
-                menuElem.classList.toggle('open');
-                return;
-            }
-            target = target.parentNode;
+    /*header__acordeon*/
+var menuElem = document.getElementById('dropdown-menu'),
+titleElem = menuElem.querySelector('.title');
+document.onclick = function(event) {
+    var target = elem = event.target;
+    while (target != this) {
+        if (target == menuElem) {
+            if(elem.tagName == 'A') titleElem.innerHTML = elem.textContent;
+            menuElem.classList.toggle('open');
+            return;
         }
-        menuElem.classList.remove('open');
+        target = target.parentNode;
     }
-}
+    menuElem.classList.remove('open');
+};
+
+/*footer__acordeon*/
+
+var accordions = document.getElementsByClassName('footer__accordion');
+
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].onclick = function() {
+            this.classList.toggle('open');
+            var content = this.nextElementSibling;
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            }
+            else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        }
+    }
+};
+
+/*scroll-up*/
+$(document).ready(function () {
+   var offset = 400;
+   var duration = 700;
+
+   $(window).scroll(function () {
+       if($(this).scrollTop() > offset) {
+           $('.scroll__up').fadeIn(duration);
+       }
+       else {
+           $('.scroll__up').fadeOut(duration);
+       }
+   });
+    $('.scroll__up').click(function () {
+        $('body').animate({scrollTop: 0},duration);
+        $('html').animate({scrollTop: 0},duration);
+    })
+});
+
